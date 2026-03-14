71a6f592563ada873a285139c7ee4030fa6d25ff
montaser <mohamed.montserLocal@gmial.com>
2026-03-14 03:01:14 +0200

    mqtt

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server on port 80
-// Note: You must run this with 'sudo' to use port 80
+// Run with 'sudo node server.js' to use port 80
 app.listen(80, '0.0.0.0', () => {
-    console.log('Calculator server running at http://10.42.0.1');
-});	
+    console.log('Control panel ready at http://192.168.1.50');
+});
