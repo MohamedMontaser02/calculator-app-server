7e8f79ed1fcd82c8d3d72fd0b03b43167b43f294
montaser <mohamed.montserLocal@gmial.com>
2026-03-14 07:13:39 +0200

    updata server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// This only serves your HTML file to the browser
+// CRITICAL: This allows the browser to find 'mqtt.min.js' in the same folder
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server on port 80
 app.listen(80, '0.0.0.0', () => {
     console.log('Server running at http://192.168.1.50');
 });
